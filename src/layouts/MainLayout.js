3702d982f2611da99492da6e1507b3b5acbd3bca
Andy Hui <chhuiwork@gmail.com>
2025-09-25 10:13:24 +0800

    Feat: Add new logo on the front page

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -61,55 +61,74 @@ const MainLayout = ({ children }) => {
     return combinedText || "Search";
   };
 
-  return (
-      <Layout style={{ minHeight: '100vh' }}>
-        <Header className="header" style={{backgroundColor: '#254000'}}>
-          <div className="logo">
-            <span className="logo-icon" style={{color: "white"}}>L'</span>
-            <span className="logo-text" style={{color: "white"}}>Travel</span>
-          </div>
-          <div className="header-center">
-            <Input
-                value={getPlaceholderText() === "Search" ? "" : getPlaceholderText()}
-                placeholder="Search"
-                prefix={<SearchOutlined />}
-                className="search-input"
-                onClick={handleSearchClick}
-                allowClear
-                onClear={handleClearSearch}
+    return (
+        <Layout style={{ minHeight: '100vh' }}>
+            <Header className="header" style={{backgroundColor: '#254000'}}>
+                <div className="logo" style={{ display: 'flex', alignItems: 'center' }}>
+                    <div style={{
+                        width: "32px",
+                        height: "32px",
+                        marginRight: "8px",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        overflow: "hidden"
+                    }}>
+                        <img
+                            src={"/logo.png"}
+                            alt={"L-travel logo"}
+                            style={{
+                                maxWidth: "100%",
+                                maxHeight: "100%",
+                                objectFit: "contain"
+                            }}
+                        />
+                    </div>
+                    <span className="logo-icon" style={{color: "white", fontSize: "18px",marginLeft: "5px"}}>L'</span>
+                    <span className="logo-text" style={{color: "white", fontSize: "18px"}}>Travel</span>
+                </div>
+                <div className="header-center">
+                    <Input
+                        value={getPlaceholderText() === "Search" ? "" : getPlaceholderText()}
+                        placeholder="Search"
+                        prefix={<SearchOutlined />}
+                        className="search-input"
+                        onClick={handleSearchClick}
+                        allowClear
+                        onClear={handleClearSearch}
+                    />
+
+                </div>
+                <div className="header-right">
+                    <Avatar icon={<UserOutlined />} />
+                    <span className="sign-up">Sign Up</span>
+                </div>
+            </Header>
+
+            <Layout>
+                <Sider width={200} className="sidebar">
+                    <Menu
+                        mode="inline"
+                        selectedKeys={[location.pathname]}
+                        items={menuItems}
+                        onClick={handleMenuClick}
+                        className="sidebar-menu"
+                    />
+                </Sider>
+
+                <Content className="main-content">
+                    {children}
+                </Content>
+            </Layout>
+
+            <CityTravelDayFilters
+                visible={showCityAndTravelDayFilter}
+                onClose={setShowCityAndTravelDayFilter}
+                onCitySelect={handleCitySelect}
+                onTravelDaysSelected={handleTravelDaySelect}
             />
-
-          </div>
-          <div className="header-right">
-            <Avatar icon={<UserOutlined />} />
-            <span className="sign-up">Sign Up</span>
-          </div>
-        </Header>
-
-        <Layout>
-          <Sider width={200} className="sidebar">
-            <Menu
-                mode="inline"
-                selectedKeys={[location.pathname]}
-                items={menuItems}
-                onClick={handleMenuClick}
-                className="sidebar-menu"
-            />
-          </Sider>
-
-          <Content className="main-content">
-            {children}
-          </Content>
         </Layout>
-
-        <CityTravelDayFilters
-            visible={showCityAndTravelDayFilter}
-            onClose={setShowCityAndTravelDayFilter}
-            onCitySelect={handleCitySelect}
-            onTravelDaysSelected={handleTravelDaySelect}
-        />
-      </Layout>
-  );
+    );
 };
 
 export default MainLayout;
